d2f0577d1783ad06763a9992f8412b3dcb84f4c8
Galse22 <al.g.se622@gmail.com>
2021-01-28 22:04:28 -0300

    Music + "Cutscenes" added

---
+++ Blackthornprod Game Jam 3/Assets/Scripts/Menu and Cutscenes/CutsceneManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CutsceneManager : MonoBehaviour
+{
+    public Sprite[] imageArray;
+    public Image baseImage;
+    public int sceneNumber;
+    public int amountsButton;
+
+    public GameObject sceneTransGO;
+    public Animator panelAnim;
+    public float loadTime;
+
+    void Awake()
+    {
+        sceneTransGO.SetActive(true);
+        panelAnim.SetTrigger("LoadedNewScene");
+    }
+
+    void Start()
+    {
+        Invoke("DeactivateGO", 1.1f);
+    }
+
+
+    public void ChangeImage()
+    {
+        amountsButton++;
+        if(amountsButton < imageArray.Length)
+        {
+            baseImage.overrideSprite = imageArray[amountsButton];
+        }
+        else
+        {
+            Invoke("ActualLoadScene", loadTime);
+            sceneTransGO.SetActive(true);
+            panelAnim.SetTrigger("GoingToNewScene");
+        }
+    }
+
+    void ActualLoadScene()
+    {
+        SceneManager.LoadScene(sceneNumber);
+    }
+
+    void DeactivateGO()
+    {
+        sceneTransGO.SetActive(false);
+    }
+}
--- Blackthornprod Game Jam 3/Assets/Scripts/Player/CollisionManagerPlayer.cs
+++ Blackthornprod Game Jam 3/Assets/Scripts/Player/CollisionManagerPlayer.cs
@@ -1,43 +1,73 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionManagerPlayer : MonoBehaviour
 {
+    [Header("Player")]
     public PlayerController playerController;
     public TimeManager timeManager;
+    public Animator playerAnim;
+
+    [Header("Death")]
     public GameObject deathGO;
     public GameObject soundDeath;
     bool changedStuff;
+    bool shouldDie = true;
+
+    [Header("Win")]
+    public GameObject sceneTransGO;
+    public Animator panelAnim;
+    public int sceneToGoInt;
+    public float timeNextScene;
+
+    [Header ("Screenshake")]
+    public float screenshakeTime;
+    public float screenshakeIntesity;
 
     void OnCollisionEnter2D(Collision2D col)
     {
-        if(col.gameObject.tag == "Spikes" || col.gameObject.tag == "Arrow")
+        if(col.gameObject.tag == "Spikes" || col.gameObject.tag == "Arrow" && shouldDie == true)
         {
             if(changedStuff == false && timeManager.isAlive == true)
             {
+                CinemachineShake.Instance.ShakeCamera(screenshakeIntesity, screenshakeTime);
                 playerController.canMove = false;
+                playerAnim.SetTrigger("Default");
                 deathGO.SetActive(true);
                 timeManager.isAlive = false;
                 Instantiate(soundDeath, this.gameObject.transform.position, Quaternion.identity);
-                Time.timeScale = 0f;
                 changedStuff = true;
             }
         }
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        if(other.gameObject.tag == "Spikes" || other.gameObject.tag == "Arrow")
+        if(other.gameObject.tag == "Spikes" || other.gameObject.tag == "Arrow" && shouldDie == true)
         {
             if(changedStuff == false && timeManager.isAlive == true)
             {
+                CinemachineShake.Instance.ShakeCamera(screenshakeIntesity, screenshakeTime);
                 playerController.canMove = false;
+                playerAnim.SetTrigger("Default");
                 deathGO.SetActive(true);
                 timeManager.isAlive = false;
                 Instantiate(soundDeath, this.gameObject.transform.position, Quaternion.identity);
-                Time.timeScale = 0f;
                 changedStuff = true;
             }
         }
+        else if(other.gameObject.tag == "Potion" || other.gameObject.tag == "Wizard")
+        {
+            shouldDie = false;
+            Invoke("NextScene", timeNextScene);
+            sceneTransGO.SetActive(true);
+            panelAnim.SetTrigger("GoingToNewScene");
+        }
+    }
+
+    void NextScene()
+    {
+        SceneManager.LoadScene(sceneToGoInt);
     }
 }
